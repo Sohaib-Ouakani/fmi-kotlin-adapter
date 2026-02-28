0e39bd759f9a8c361b70efbfc47f83d0d762955a
Sohaib Ouakani <sohaib.ouakani@studio.unibo.it>
2026-02-28 12:33:32 +0100

    print successfully info about FMU

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,23 +1,31 @@
+import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import libfmi.
+import kotlinx.cinterop.toKString
+import libfmi.*
 
-@Serializable
-private data class Message(
-    val topic: String,
-    val content: String,
-)
+const val DIR = "./resources/"
 
-private val PrettyPrintJson = Json {
-    prettyPrint = true
-}
 
 @OptIn(ExperimentalForeignApi::class)
 fun main() {
-    val message = Message(
-        topic = "Kotlin/Native",
-        content = "Hello!"
+    val context: CPointer<fmi_import_context_t>? = fmi_import_allocate_context(null)
+    val verison = fmi_import_get_fmi_version(
+        context,
+        "./resources/BouncingBall.fmu",
+        "./resources/"
     )
-    println(PrettyPrintJson.encodeToString(message))
+    val characteristics: MutableMap<String, String?> = mutableMapOf()
+
+    val fmi = fmi2_import_parse_xml(context, DIR, null)
+
+    characteristics["Model Name"] = fmi2_import_get_model_name(fmi)?.toKString()
+    characteristics["Description"] = fmi2_import_get_description(fmi)?.toKString()
+    characteristics["Author"] = fmi2_import_get_author(fmi)?.toKString()
+    characteristics["FMU Version"] = fmi2_import_get_model_version(fmi)?.toKString()
+    characteristics["Default Experiment Start"] = fmi2_import_get_default_experiment_start(fmi).toString()
+    characteristics["Default Experiment Stop"] = fmi2_import_get_default_experiment_stop(fmi).toString()
+    characteristics["Default Experiment Step"] = fmi2_import_get_default_experiment_step(fmi).toString()
+    characteristics["FMU type"] = fmi2_import_get_fmu_kind(fmi).toString()
+
+    print(characteristics)
 }
